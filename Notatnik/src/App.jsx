e70721325ec953968f17babe643fcd0d60f4a045
Kajakkkkkk <u100_KajDej_waw@technischools.com>
2024-05-12 00:05:25 +0200

    routing

--- Notatnik/src/App.jsx
+++ Notatnik/src/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
 import Note from "./components/note";
-import NotFound from "./components/notFound";
+
+function NotFound() {
+  return (
+    <div>
+      <h1>Nie znaleziono strony</h1>
+      <p>Przepraszamy, ale podany adres jest niepoprawny.</p>
+      <a href="/">Przejdź do poprawnej strony</a>
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Note />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/404" />} />
       </Routes>
     </Router>
   );
